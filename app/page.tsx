60e65196e6afb4cf3e74cdb1764b8cb8c1a9c450
Sidney Pang <sidneypang@hotmail.co.uk>
2023-06-21 23:32:10 -0400

    made main card

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import CityPicker from "@/components/CityPicker";
+import { Card, Divider, Subtitle, Text } from "@tremor/react";
+
 export default function Home() {
   return (
-    <main className="">
-      <h1>lets build</h1>
-    </main>
-  )
+    <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center">
+      <Card className="max-w-4xl mx-auto">
+        <Text className="text-5xl font-bold text-center mb-10">Weather AI</Text>
+        <Subtitle className="text-xl text-center">
+          Powered by OpenAI, Next.js, Tailwind CSS, Tremor and more!
+        </Subtitle>
+
+        <Divider className="my-10" />
+
+        <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
+          <CityPicker />
+        </Card>
+      </Card>
+    </div>
+  );
 }
